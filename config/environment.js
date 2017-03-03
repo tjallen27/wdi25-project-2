0625581d2ec7665b052a34ea4d85e2216872aeaa
tjallen27 <tjallen91@hotmail.co.uk>
2017-03-03 10:00:36 +0000

    Updating mongodb uri

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'development';
-const dbURI = process.env.MONGO_URI || `mongodb://localhost/PROJECT_NAME-${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/project-2-${env}`;
 const sessionSecret = process.env.SESSION_SECRET || 'my awesome secret';
 
 module.exports = { port, env, dbURI, sessionSecret };
